ee968521f7bd10c37f01276e9bbe4c002de4e7c6
Altimea Altimea <ext.diego.leguia@renta4.pe>
2025-04-28 22:10:52 -0500

    feat: cargar remoteEntry.js dinámico desde parámetro URL

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,22 @@
 import { loadRemoteModule } from '@angular-architects/module-federation';
 import { Routes } from '@angular/router';
 
+function getRemoteEntryUrl(): string {
+  const urlParams = new URLSearchParams(window.location.search);
+  const remoteUrl = urlParams.get('remote');
+  if (!remoteUrl) {
+    throw new Error('No se encontró el parámetro ?remote en la URL.');
+  }
+  return remoteUrl;
+}
+
 export const routes: Routes = [
   {
     path: '',
     loadComponent: () =>
       loadRemoteModule({
         type: 'module',
-        remoteEntry: 'http://localhost:4201/remoteEntry.js',
+        remoteEntry: getRemoteEntryUrl(),
         exposedModule: './HomeComponent',
       }).then((m: any) => m.HomeComponent),
   },
